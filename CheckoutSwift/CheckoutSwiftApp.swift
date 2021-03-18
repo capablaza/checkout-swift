6d059a304f6d689f9baa2b2e8cd335b03be33480
carlos apablaza <c.apablaza.z@gmail.com>
2021-03-18 09:13:36 -0300

    Initial Commit

---
+++ CheckoutSwift/CheckoutSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckoutSwiftApp.swift
+//  CheckoutSwift
+//
+//  Created by carlos alberto apablaza zamorano on 18-03-21.
+//
+
+import SwiftUI
+
+@main
+struct CheckoutSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
